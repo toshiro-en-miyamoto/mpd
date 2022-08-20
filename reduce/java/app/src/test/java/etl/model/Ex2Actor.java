411293b80408ba30165d39fda9a493c8aa226186
Toshiro Miyamoto <toshiro.en.miyamoto@gmail.com>
2022-08-20 22:57:03 +0800

    test data finished

---
+++ reduce/java/app/src/test/java/etl/model/Ex2Actor.java
@@ -0,0 +1,12 @@
+package etl.model;
+
+public interface Ex2Actor {
+    static String text
+    = "bf6edc9b,Anne Hathaway,1982-11-12,\n"
+    + "b18c86f9,Clint Eastwood,1930-05-31,\n"
+    + "1d1aac52,Meryl Streep,1949-06-22,\n"
+    + "b018008a,Rene Russo,1954-02-17,\n"
+    + "089dac8a,Robert De Niro,1943-08-17,\n"
+    + "86c9cdb3,Robin Williams,1951-07-21,2014\n"
+    ;
+}
---
+++ reduce/java/app/src/test/java/etl/model/Ex2Film.java
@@ -0,0 +1,10 @@
+package etl.model;
+
+public interface Ex2Film {
+    static String text
+    = "e59ef957,The Deer Hunter,1978\n"
+    + "e26705b0,\"Good Morning, Vietnam\",1987\n"
+    + "615df8f4,The Bridges of Madison County,1995\n"
+    + "095fe76f,The Intern,2015\n"
+    ;
+}
